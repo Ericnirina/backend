e1214cba89af4c6dfdbb156d2f521a3783c1afc0
manoa11 <manoaharinjo@gmail.com>
2022-11-12 15:38:33 +0200

    maj

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: 'ADMIN_JWT_SECRET',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: 'API_TOKEN_SALT',
   },
 });
